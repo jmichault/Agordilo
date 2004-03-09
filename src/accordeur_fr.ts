b024a3ae160e5cf46d87d0927a0e1f97d0c62e0b
jmichault <48726908+jmichault@users.noreply.github.com>
2004-03-09 19:41:31 +0000

    added localization

---
+++ src/accordeur_fr.ts
@@ -0,0 +1,81 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>Form1</name>
+    <message>
+        <source>Accordeur</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Calibrate background sound</source>
+        <translation type="unfinished">Calibrer le bruit de fond</translation>
+    </message>
+    <message>
+        <source>A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A#</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>C#</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>D#</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>E</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>F</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>F#</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>G</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>G#</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>00000.00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Frequence :</source>
+        <translation type="unfinished">Fréquence :</translation>
+    </message>
+    <message>
+        <source>XXX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pitch :</source>
+        <translation type="unfinished">Note :</translation>
+    </message>
+    <message>
+        <source>Octave :</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
