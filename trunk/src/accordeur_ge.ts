171093ca95ee506711b77c8247f9156bde03eeef
jmichault <jmichault@4fe7c420-d127-0410-972f-e538c664a2ba>
2005-04-16 21:54:22 +0000

    initial translation

---
+++ trunk/src/accordeur_ge.ts
@@ -0,0 +1,212 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>FAbout</name>
+    <message>
+        <source>About...</source>
+        <translation>Über...</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&lt;p align=&quot;center&quot;&gt;- Accordeur -&lt;/p&gt;
+&lt;p align=&quot;center&quot;&gt;Instrument tuner&lt;/p&gt;
+&lt;p align=&quot;center&quot;&gt;Version 0.4.0&lt;/p&gt;
+&lt;p align=&quot;center&quot;&gt;License : GPL&lt;/p&gt;
+&lt;p align=&quot;center&quot;&gt;http://accordeur.sourceforge.net&lt;/p&gt;
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FHelp</name>
+    <message>
+        <source>Form2</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Form1</name>
+    <message>
+        <source>Accordeur</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>00000.00</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Frequence :</source>
+        <translation>Frequenz :</translation>
+    </message>
+    <message>
+        <source>XXX</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Found Pitch :</source>
+        <translation>gefundener Taktabstand :</translation>
+    </message>
+    <message>
+        <source>Perfect :</source>
+        <translation>vollkommen :</translation>
+    </message>
+    <message>
+        <source>Piano</source>
+        <translation>Klavier</translation>
+    </message>
+    <message>
+        <source>Octave :</source>
+        <translation>Oktave :</translation>
+    </message>
+    <message>
+        <source>C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>C#</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>D</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>D#</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>E</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>F</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>F#</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>G</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>G#</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>A#</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>B</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Others</source>
+        <translation>Andere</translation>
+    </message>
+    <message>
+        <source>Strings</source>
+        <translation>Seile</translation>
+    </message>
+    <message>
+        <source>E2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>A2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>D3</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>G3</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>B3</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>E4</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Guitar 6 strings</source>
+        <translation>Gitarre 6 Seile</translation>
+    </message>
+    <message>
+        <source>0000.00</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>diff. :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>cent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>near desired pitch :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Calibrate background noise</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Push this to evaluate background&apos;s spectrum !</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Contents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Contents...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Index</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Index...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
