d62919342d020b5277a3289881ddee144c8b2f93
jmichault <48726908+jmichault@users.noreply.github.com>
2006-01-29 13:32:59 +0000

    *** empty log message ***

--- src/accordeur_ge.ts
+++ src/accordeur_ge.ts
@@ -200,4 +200,34 @@
         <translation type="unfinished"></translation>
     </message>
 </context>
+<context>
+    <name>Instruments</name>
+    <message> <source>Guitare 6 strings standard</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Balalaika Prima</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Balalaika Secunda</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Balalaika Alto</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Balalaika Bass</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Balalaika ContraBass</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Banjo 4-String Tenor</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Banjo 4-String Irish Tenor</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Banjo 4-String Plectrum</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Banjo 4-String Guitar</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Banjo 4-String Open G</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Banjo G</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Banjo C</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Banjo D</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Banjo Double C</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Bass Guitar 4 strings Standard</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Bass Guitar 5-String Standard</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Bass Guitar 6-String Standard</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Bass Guitar 7-String Standard</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Bass Guitar 8-String Standard</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Bouzouki Greek</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Bouzouki Irish</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Charango</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Mandolin</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Violin</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Viola</source> <translation type="unfinished"></translation> </message>
+    <message> <source>Cello</source> <translation type="unfinished"></translation> </message>
+</context>
 </TS>
